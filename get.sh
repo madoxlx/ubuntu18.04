591e7771dc833c2d1ddcd88c8bc990f7701f9ff6
madoxlx <153240865+madoxlx@users.noreply.github.com>
2023-12-08 12:54:24 +0200

    Update get.sh

--- get.sh
+++ get.sh
@@ -2,7 +2,6 @@ wget -O ng.sh https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine/raw/ma
 chmod +x ng.sh
 ./ng.sh
 
-
 function goto
 {
     label=$1
@@ -34,8 +33,10 @@ echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 4000 &>/dev/null &
 sleep 1
-if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
-docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
+
+# Replace the existing docker run command with the updated one
+docker run --detach --privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:rw --cgroupns=host --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user geerlingguy/docker-ubuntu1804-ansible:latest
+
 clear
 echo "NoMachine: https://www.nomachine.com/download"
 echo Done! NoMachine Information:
